5458139dd917cb260df76f0ce0d644b3951edb11
Faishal <ishal.2401@gmail.com>
2024-09-03 13:53:31 +0700

    Button notification test

--- src/Pages/Setting.js
+++ src/Pages/Setting.js
@@ -3,6 +3,7 @@ import { TouchableOpacity } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { Button, Icon, Div, Header, Image, Text } from "react-native-magnus";
 import toCamelCase from "../../utils/camelCase";
+import notificationHandler from "../../utils/notificationHandler";
 const logo = require("./../../assets/splash2.png");
 
 const Setting = ({ navigation }) => {
@@ -115,6 +116,39 @@ const Setting = ({ navigation }) => {
               />
               <Div justifyContent="center" ml={10}>
                 <Text fontSize="lg">Logout</Text>
+              </Div>
+            </Div>
+
+            <Icon
+              name="keyboard-arrow-right"
+              fontFamily="MaterialIcons"
+              fontSize={25}
+              color="#2E3A59"
+            />
+          </Div>
+        </TouchableOpacity>
+
+        {/* Test Notification */}
+        <TouchableOpacity onPress={notificationHandler}>
+          <Div
+            p={15}
+            bg="#D8DEF3"
+            rounded={10}
+            borderWidth={1}
+            borderColor="#b3b8c9"
+            row
+            justifyContent="space-between"
+            mt={20}
+          >
+            <Div row>
+              <Icon
+                name="notifications-active"
+                fontFamily="MaterialIcons"
+                fontSize={25}
+                color="blue600"
+              />
+              <Div justifyContent="center" ml={10}>
+                <Text fontSize="lg">Notification</Text>
               </Div>
             </Div>
 
